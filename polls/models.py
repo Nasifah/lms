eb465181d5ef00c27adfafc228aed2b87f9f3f19
Nasifah <83665329+Nasifah@users.noreply.github.com>
2022-07-16 03:25:50 +0100

    polls: Update Polls Models and make Migrations

--- polls/models.py
+++ polls/models.py
@@ -4,9 +4,13 @@ from django.db import models
 # Create your models here.
 
 class Question(models.Model):
-    id = models.PrimaryKey(auto_increment=True) #not necessary to write this line as django automatically increases the numbering, you can also write it while leaving the brackets empty
+   # id = models.PrimaryKey(auto_increment=True) #not necessary to write this line as django automatically increases the numbering, you can also write it while leaving the brackets empty
     Question_text = models.CharField(max_length=255)
+    Question_description = models.CharField(max_length=255, default="Basic Description") #so former Questions will have Basic Description in this new field
     pub_date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):  #this is Python related and not database related, it replaces default object title with actual title 
+        return self.Question_text
 
     #so the below is what you are hoping to achieve with the above 
     #  1)        "Are you happy about Django?"       "June 16, 09:50 WAT"
@@ -18,6 +22,8 @@ class Choices(models.Model):
     choice_answer = models.CharField(max_length=20)
     votes = models.IntegerField(default=0) #default makes it 0/0 for a start before 1/0 - meaning first yes
 
+    def __str__(self):
+        return self.choice_answer
+
     #  id        Question                         choice_answer      votes
     #  1)    "Are you happy about Django"         yes/no             1/0
-    
